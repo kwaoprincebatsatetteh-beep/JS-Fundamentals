6e9d95dd2bf638a4cd147a4b62e4392a13101512
Kwao Prince Batsatetteh <kwaoprincebatsatetteh@gmail.com>
2025-11-10 08:09:56 -0600

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -2,6 +2,6 @@ const line1 = "C is fun";
 const line2 = "python is cool";
 const line3 = "JavaScript is amazing"
 
-console.log (line1);
-console.log (line2);
-console.log (line3);
+console.log(line1);
+console.log(line2);
+console.log(line3);
